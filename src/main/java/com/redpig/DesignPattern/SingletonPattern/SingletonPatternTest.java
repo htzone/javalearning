d1cccfd48b7ecd435ecfeb4fa0bd0aeb1bab4368
htzone <877526016@qq.com>
2018-07-05 21:43:55 +0800

    java learning

--- src/main/java/com/redpig/DesignPattern/SingletonPattern/SingletonPatternTest.java
+++ src/main/java/com/redpig/DesignPattern/SingletonPattern/SingletonPatternTest.java
@@ -6,6 +6,7 @@ package com.redpig.DesignPattern.SingletonPattern;
 public class SingletonPatternTest {
     public static void main(String[] args){
         FileOpener1.INSTANCE.openFile();
+
         for(int i =0; i<10;i++){
             FileOpener2.getInstance().openFile();
         }
