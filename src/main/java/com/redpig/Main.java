31182ad5caf878b0955a0c65bff28d4010e29e3c
htzone <877526016@qq.com>
2018-12-20 21:18:03 +0800

    java learning...

--- src/main/java/com/redpig/Main.java
+++ src/main/java/com/redpig/Main.java
@@ -1,10 +1,16 @@
 package com.redpig;
 
+import com.redpig.common.Service.BaseService;
+import com.redpig.common.spring.AppCtxHolder;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by hetao on 2018/7/5.
  */
 public class Main {
     public static void main(String[] args){
         System.out.println("hello world!");
+        new ClassPathXmlApplicationContext("context.xml");
+        BaseService baseService = AppCtxHolder.getBean("baseService");
     }
 }
